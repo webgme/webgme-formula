708364cfc2d94c6cd3d1ba4f78051ff4f9d2b59d
Zsolt Lattmann <zsolt.lattmann@gmail.com>
2016-02-05 08:46:47 -0600

    Use webgme@1.4.1, localhost:8228, and webgme_formula database.

--- config/config.default.js
+++ config/config.default.js
@@ -4,8 +4,8 @@ var config = require('./config.webgme'),
     validateConfig = require('webgme/config/validator');
 
 // Add/overwrite any additional settings here
-// config.server.port = 8080;
-// config.mongo.uri = mongodb://127.0.0.1:27017/webgme_my_app;
+config.server.port = 8228;
+config.mongo.uri = 'mongodb://127.0.0.1:27017/webgme_formula';
 
 //config.requirejsPaths.jsonld = './node_modules/jsonld/js/jsonld';
 
