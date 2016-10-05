8f22c292a4f182410788bd3d23000bd636273ee4
Patrik Meijer <patrik85@isis.vanderbilt.edu>
2016-10-05 10:33:36 -0500

    Create config.win.js

---
+++ config/config.win.js
@@ -0,0 +1,37 @@
+'use strict';
+
+var config = require('./config.webgme'),
+    validateConfig = require('webgme/config/validator');
+
+// Add/overwrite any additional settings here
+config.server.port = 8228;
+config.mongo.uri = 'mongodb://127.0.0.1:27017/formula';
+// config.mongo.uri = 'mongodb://127.0.0.1:27017/webgme';
+
+//config.requirejsPaths.jsonld = './node_modules/jsonld/js/jsonld';
+
+config.plugin.allowServerExecution = true;
+
+config.webhooks.enable = true;
+
+// We do not want to spam the console with bin script logs.
+config.bin.log.transports = [{
+    transportType: 'File',
+    options: {
+        name: 'info-file',
+        filename: './server.log',
+        level: 'info',
+        json: false
+    }
+}, {
+    transportType: 'File',
+    options: {
+        name: 'error-file',
+        filename: './server-error.log',
+        level: 'error',
+        handleExceptions: true,
+        json: false
+    }
+}];
+validateConfig(config);
+module.exports = config;
